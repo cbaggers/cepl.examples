1d55a7e89e048010877eb56a5a42ac20436809a1
Baggers <chris.bagley@gmail.com>
2016-04-10 16:47:41 +0200

    bloom works again

--- examples/bloom.lisp
+++ examples/bloom.lisp
@@ -3,9 +3,21 @@
 ;; NOTE: Ensure you have loaded cepl-image-helper (or cepl-default)
 
 (defvar cols nil)
+(defvar cols-sampler nil)
+
 (defvar *loop* 0)
-(defvar *quad* nil)
-(defvar *quad-stream* nil)
+(defvar *quad*
+  (make-gpu-array
+   (list (list (v! -1.0   1.0 0 0) (v!  0.0   1.0))
+	 (list (v! -1.0  -1.0 0 0) (v!  0.0   0.0))
+	 (list (v!  1.0  -1.0 0 0) (v!  1.0   0.0))
+	 (list (v! -1.0   1.0 0 0) (v!  0.0   1.0))
+	 (list (v!  1.0  -1.0 0 0) (v!  1.0   0.0))
+	 (list (v!  1.0   1.0 0 0) (v!  1.0   1.0)))
+   :element-type 'g-pt
+   :dimensions 6))
+(defvar *quad-stream*
+  (make-buffer-stream *quad* :retain-arrays t))
 
 (defun-g vert ((quad g-pt))
   (values (v! (pos quad) 1) (tex quad)))
@@ -15,7 +27,8 @@
 (defun-g frag ((tc :vec2) &uniform (tex :sampler-2d))
   (texture tex tc))
 
-(defpipeline blit () (g-> #'vert #'frag))
+(def-g-> blit ()
+  #'vert #'frag)
 
 ;;- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -24,7 +37,8 @@
      (* (texture tex tc) 0.375)
      (* (texture tex (+ tc offset)) 0.3125)))
 
-(defpipeline smooth () (g-> #'vert #'qkern))
+(def-g-> smooth ()
+  #'vert #'qkern)
 
 ;;- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -36,42 +50,61 @@
        (* (texture t2 tc) scale-effect)
        (* (texture t3 tc) scale-effect))))
 
-(defpipeline combine () (g-> #'vert #'fourtex))
+(def-g-> combine ()
+  #'vert #'fourtex)
 
 ;;- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-(defpipeline bloom (stream &uniform tx)
-    (g-> (c0 (blit stream :tex tx))
-         (c1 (blit stream :tex tx))
-         (c2 (blit stream :tex tx))
-         (c3 (blit stream :tex tx))
-         (h0 (smooth stream :tex (attachment c0 0) :offset (v! (/ 1.2 512) 0)))
-         (h1 (smooth stream :tex (attachment c1 0) :offset (v! (/ 1.2 256) 0)))
-         (h2 (smooth stream :tex (attachment c2 0) :offset (v! (/ 1.2 128) 0)))
-         (h3 (smooth stream :tex (attachment c3 0) :offset (v! (/ 1.2 64) 0)))
-         (c0 (smooth stream :tex (attachment h0 0) :offset (v! 0 (/ 1.2 512))))
-         (c1 (smooth stream :tex (attachment h1 0) :offset (v! 0 (/ 1.2 256))))
-         (c2 (smooth stream :tex (attachment h2 0) :offset (v! 0 (/ 1.2 128))))
-         (c3 (smooth stream :tex (attachment h3 0) :offset (v! 0 (/ 1.2 64))))
-         (nil (combine stream
-                       :t0 (attachment c0 0) :t1 (attachment c1 0)
-                       :t2 (attachment c2 0) :t3 (attachment c3 0))))
-  :fbos
-  (c0 '(:c :dimensions (512 512) :magnify-filter :linear))
-  (c1 '(:c :dimensions (256 256) :magnify-filter :linear))
-  (c2 '(:c :dimensions (128 128) :magnify-filter :linear))
-  (c3 '(:c :dimensions (64 64) :magnify-filter :linear))
-  (h0 '(:c :dimensions (512 512)))
-  (h1 '(:c :dimensions (256 256)))
-  (h2 '(:c :dimensions (128 128)))
-  (h3 '(:c :dimensions (64 64))))
+(defvar* fbos
+  (c0 (make-fbo '(:c :dimensions (512 512))))
+  (sc0 (sample (attachment-tex c0 0)))
+  (c1 (make-fbo '(:c :dimensions (256 256))))
+  (sc1 (sample (attachment-tex c1 0)))
+  (c2 (make-fbo '(:c :dimensions (128 128))))
+  (sc2 (sample (attachment-tex c2 0)))
+  (c3 (make-fbo '(:c :dimensions (64 64))))
+  (sc3 (sample (attachment-tex c3 0)))
+  (h0 (make-fbo '(:c :dimensions (512 512))))
+  (sh0 (sample (attachment-tex h0 0)))
+  (h1 (make-fbo '(:c :dimensions (256 256))))
+  (sh1 (sample (attachment-tex h1 0)))
+  (h2 (make-fbo '(:c :dimensions (128 128))))
+  (sh2 (sample (attachment-tex h2 0)))
+  (h3 (make-fbo '(:c :dimensions (64 64))))
+  (sh3 (sample (attachment-tex h3 0))))
+
+(defun bloom (stream sx)
+  (map-g-into (fbos-c0 fbos) #'blit stream :tex sx)
+  (map-g-into (fbos-c1 fbos) #'blit stream :tex sx)
+  (map-g-into (fbos-c2 fbos) #'blit stream :tex sx)
+  (map-g-into (fbos-c3 fbos) #'blit stream :tex sx)
+  (map-g-into (fbos-h0 fbos) #'smooth stream
+	      :tex (fbos-sc0 fbos) :offset (v! (/ 1.2 512) 0))
+  (map-g-into (fbos-h1 fbos) #'smooth stream
+	      :tex (fbos-sc1 fbos) :offset (v! (/ 1.2 256) 0))
+  (map-g-into (fbos-h2 fbos) #'smooth stream
+	      :tex (fbos-sc2 fbos) :offset (v! (/ 1.2 128) 0))
+  (map-g-into (fbos-h3 fbos) #'smooth stream
+	      :tex (fbos-sc3 fbos) :offset (v! (/ 1.2 64) 0))
+  (map-g-into (fbos-c0 fbos) #'smooth stream
+	      :tex (fbos-sh0 fbos) :offset (v! 0 (/ 1.2 512)))
+  (map-g-into (fbos-c1 fbos) #'smooth stream
+	      :tex (fbos-sh1 fbos) :offset (v! 0 (/ 1.2 256)))
+  (map-g-into (fbos-c2 fbos) #'smooth stream
+	      :tex (fbos-sh2 fbos) :offset (v! 0 (/ 1.2 128)))
+  (map-g-into (fbos-c3 fbos) #'smooth stream
+	      :tex (fbos-sh3 fbos) :offset (v! 0 (/ 1.2 64)))
+  (map-g #'combine stream
+	 :t0 (fbos-sc0 fbos) :t1 (fbos-sc1 fbos)
+	 :t2 (fbos-sc2 fbos) :t3 (fbos-sc3 fbos)
+	 :scale-effect (abs (sin *loop*))))
 
 (defun step-demo ()
   (incf *loop* 0.01)
   (step-host)
   (update-repl-link)
   (clear)
-  (map-g #'bloom *quad-stream* :tx cols :scale-effect (abs (sin *loop*)))
+  (bloom *quad-stream* cols-sampler)
   (swap))
 
 ;;-------------------------------------------------------
@@ -82,16 +115,7 @@
   (unless cols
     (setf cols (cepl.devil:load-image-to-texture
                 (merge-pathnames "ThickCloudsWater/front.png" *examples-dir*)))
-    (setf *quad* (make-gpu-array
-                  (list (list (v! -1.0   1.0 0 0) (v!  0.0   1.0))
-                        (list (v! -1.0  -1.0 0 0) (v!  0.0   0.0))
-                        (list (v!  1.0  -1.0 0 0) (v!  1.0   0.0))
-                        (list (v! -1.0   1.0 0 0) (v!  0.0   1.0))
-                        (list (v!  1.0  -1.0 0 0) (v!  1.0   0.0))
-                        (list (v!  1.0   1.0 0 0) (v!  1.0   1.0)))
-                  :element-type 'g-pt
-                  :dimensions 6))
-    (setf *quad-stream* (make-buffer-stream *quad* :retain-arrays t)))
+    (setf cols-sampler (sample cols)))
   (loop :while *running* :do (continuable (step-demo))))
 
 (defun stop-loop ()
