6934e99b5ea46266c5ab0fe67a481a9fe0eed8ba
cbaggers <chris.bagley@gmail.com>
2017-11-12 20:50:19 +0100

    improving the transform-feedback example

--- examples/transform-feedback.lisp
+++ examples/transform-feedback.lisp
@@ -1,7 +1,7 @@
 (in-package :cepl.examples)
 
 (defvar *running* nil)
-(defvar *vertex-stream* nil)
+(defparameter *vertex-stream* nil)
 (defvar *feedback-vec3* nil)
 (defvar *feedback-vec4* nil)
 (defvar *tfs* nil)
@@ -10,15 +10,15 @@
   (unless *vertex-stream*
     (setf *vertex-stream*
           (make-buffer-stream
-           (make-gpu-array (list (v!  0.0   0.7  0.0  1.0)
-                                 (v! -0.7  -0.7  0.0  1.0)
-                                 (v!  0.7  -0.7  0.0  1.0))
+           (make-gpu-array (list (v!  0.0   0.5  0.0  1.0)
+                                 (v! -0.5  -0.5  0.0  1.0)
+                                 (v!  0.5  -0.5  0.0  1.0))
                            :element-type :vec4
                            :dimensions 3)))
     (setf *feedback-vec3*
           (make-gpu-array nil :element-type :vec3 :dimensions 10))
     (setf *feedback-vec4*
-          (make-gpu-array nil :element-type :vec3 :dimensions 10))
+          (make-gpu-array nil :element-type :vec4 :dimensions 10))
     (setf *tfs*
           (make-transform-feedback-stream *feedback-vec3*
                                           *feedback-vec4*)))
@@ -26,7 +26,7 @@
 
 (defun-g mtri-vert ((position :vec4) &uniform (pos :vec2))
   (values ((:feedback 1) (+ position (v! pos 0 0)))
-          ((:feedback 0) (v! 0.3 0 1))))
+          ((:feedback 0) (v! 0.1 0 1))))
 
 (defun-g mtri-frag ((col :vec3))
   (v! col 0))
@@ -40,16 +40,21 @@
   (update-repl-link)
   (clear)
   (with-transform-feedback (*tfs*)
-    (map-g #'prog-1 *vertex-stream* :pos (v! 0 0))
-    (map-g #'prog-1 *vertex-stream* :pos (v! 0.5 0.5)))
+    (map-g #'prog-1 *vertex-stream* :pos (v! -0.1 0)))
+  (map-g #'prog-1 *vertex-stream* :pos (v! 0.3 0.28))
   (swap))
 
 
 (defun run-loop ()
   (unless *running*
-    (setf *running* t)
-    (reset)
-    (loop :while (and *running* (not (shutting-down-p))) :do
-       (continuable (step-demo)))))
+    (print "--- started ---")
+    (unwind-protect
+         (progn
+           (setf *running* t)
+           (reset)
+           (loop :while (and *running* (not (shutting-down-p))) :do
+              (continuable (step-demo))))
+      (setf *running* nil)
+      (print "--- stopped ---"))))
 
 (defun stop-loop () (setf *running* nil))
