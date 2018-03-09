32e812975d9be743b24237bee65f0afe354e1c8c
Baggers <chris.bagley@gmail.com>
2018-03-09 14:41:35 +0100

    compute example (untested)

---
+++ examples/compute.lisp
@@ -0,0 +1,46 @@
+(in-package :cepl.examples)
+
+(defstruct-g (ssbo-test-data :layout std-430)
+  (vals (:int 30)))
+
+(defvar *ssbo* nil)
+(defvar *running* nil)
+
+(defun-g yay-compute (&uniform
+                      (woop ssbo-test-data :ssbo)
+                      (modifier :float))
+  (declare (local-size :x 1 :y 1 :z 1))
+  (setf (aref (ssbo-test-data-vals woop)
+              (int (x gl-work-group-id)))
+        (int (* (x gl-work-group-id) modifier)))
+  (values))
+
+(defpipeline-g run-compute ()
+  :compute yay-compute)
+
+(defun init ()
+  (unless *ssbo*
+    (let ((arr (make-gpu-array
+                nil
+                :dimensions 1
+                :element-type 'ssbo-test-data)))
+      (setf *ssbo* (make-ssbo arr)))))
+
+(defun step-example (modifier)
+  (map-g #'run-compute (make-compute-space 30) :woop *ssbo*
+         :modifier (float modifier 0f0))
+  (let ((fence (make-gpu-fence)))
+    (wait-on-gpu-fence fence)
+    (free fence)
+    (print (pull-g (ssbo-data *ssbo*)))
+    (sleep 1)))
+
+(defun run-loop ()
+  (setf *running* t)
+  (init)
+  (loop :while (and *running* (not (shutting-down-p)))
+     :for modifier :from 0
+     :do (continuable (step-example modifier))))
+
+(defun stop-loop ()
+  (setf *running* nil))
