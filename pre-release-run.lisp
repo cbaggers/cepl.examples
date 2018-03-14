098616ddfefdadc898b7f50a86e30ca7182d93bd
cbaggers <chris.bagley@gmail.com>
2018-03-14 01:32:12 +0100

    say which file we are compiling in pre-release-run

--- pre-release-run.lisp
+++ pre-release-run.lisp
@@ -14,6 +14,7 @@
              (compile-file path)
              (load path))
            (print "----------------------------------------------------------")
+           (print path)
            (print "starting")
            (let ((std-o *standard-output*))
              (bt:make-thread
